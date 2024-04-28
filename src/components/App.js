73e4a9eb5d78dca787669f4f8a3aae2e3bb0a8b8
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-28 13:10:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,65 @@
-
-import React from "react";
-import './../styles/App.css';
+// App.js
+import React, { useState } from 'react';
+import Step from './Step';
 
 const App = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    model: '',
+    car_price: '',
+    card_info: '',
+    expiry_date: '',
+  });
+
+  const nextStep = () => {
+    return new Promise((resolve) => {
+      setStep((prevStep) => {
+        const newStep = prevStep + 1;
+        resolve(newStep);
+        return newStep;
+      });
+    });
+  };
+
+  const prevStep = () => {
+    return new Promise((resolve) => {
+      setStep((prevStep) => {
+        const newStep = prevStep - 1;
+        resolve(newStep);
+        return newStep;
+      });
+    });
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = () => {
+    // Here you can handle form submission, 
+    console.log('Form submitted:', formData);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Step
+        step={step}
+        formData={formData}
+        handleChange={handleChange}
+        nextStep={nextStep}
+        prevStep={prevStep}
+        handleSubmit={handleSubmit}
+      />
+      <div id='step2'></div>
+    <div id='step3'></div>
+    <button type="button"></button>
     </div>
-  )
-}
+    
 
-export default App
+  );
+};
+
+export default App;
